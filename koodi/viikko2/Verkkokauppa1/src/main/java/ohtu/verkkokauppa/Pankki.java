5d6600022f8651851ba1e3242d71738eb449995a
EssKayz <esskay@protonmail.com>
2018-11-25 10:31:36 +0200

    Commit for verkkokauppa
    MODIFIED >
    Changed project according to instructions

--- koodi/viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Pankki.java
+++ koodi/viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Pankki.java
@@ -1,24 +1,16 @@
 package ohtu.verkkokauppa;
 
-public class Pankki {
+public class Pankki implements PankkiInterface {
 
-    private static Pankki instanssi;
+    private KirjanpitoInterface kirjanpito;
 
-    public static Pankki getInstance() {
-        if (instanssi == null) {
-            instanssi = new Pankki();
-        }
-
-        return instanssi;
-    }
-    private Kirjanpito kirjanpito;
-
-    public Pankki() {
-        kirjanpito = Kirjanpito.getInstance();
+    public Pankki(KirjanpitoInterface kirjanpito) {
+        this.kirjanpito = kirjanpito;
     }
 
+    @Override
     public boolean tilisiirto(String nimi, int viitenumero, String tililta, String tilille, int summa) {
-        kirjanpito.lisaaTapahtuma("tilisiirto: tililtä " + tilille + " tilille " + tilille
+        kirjanpito.lisaaTapahtuma("tilisiirto: tililtä " + tililta + " tilille " + tilille
                 + " viite " + viitenumero + " summa " + summa + "e");
 
         // täällä olisi koodi joka ottaa yhteyden pankin verkkorajapintaan
